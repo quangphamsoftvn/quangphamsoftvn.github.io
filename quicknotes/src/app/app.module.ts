1f66d1fd4b3d85971836e4401ff2a3a8b6b39f74
Quang Pham <quang.pham@axonactive.com>
2017-09-13 17:24:09 +0700

    add component

--- quicknotes/src/app/app.module.ts
+++ quicknotes/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NotesComponent } from './notes/notes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesComponent
   ],
   imports: [
     BrowserModule,
