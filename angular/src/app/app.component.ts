26c364850c6441d89fef217b05b377121aa8590b
Quang Pham <quang.pham@axonactive.com>
2017-09-19 08:30:28 +0700

    add new table

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,93 +1,292 @@
 import { Component } from '@angular/core';
+
+
+
 declare var fabric: any;
+
+
+
 @Component({
+
   selector: 'app-root',
+
   templateUrl: './app.component.html',
+
   styleUrls: ['./app.component.css']
+
 })
+
 export class AppComponent {
+
   title = 'app';
-  canvas: any;
+
+  __canvas: any = null;
+
+  canvas: any = null;
+
+  add() {
+
+    var red = new fabric.Rect({
+
+      top: 100, left: 0, width: 80, height: 50, fill: 'red'
+
+    });
+
+    var blue = new fabric.Rect({
+
+      top: 0, left: 100, width: 50, height: 70, fill: 'blue'
+
+    });
+
+    var green = new fabric.Rect({
+
+      top: 100, left: 100, width: 60, height: 60, fill: 'green'
+
+    });
+
+    this.canvas.add(red, blue, green);
+
+  }
+
+
+
+
+
+
+
   constructor() {
 
+
+
     setTimeout(() => {
-      this.int();
+
+      this.init();
+
     }, 500);
+
+
+
   }
 
-  addTable() {
+
+
+  init() {
+
+
+
+     this.canvas = this.__canvas = new fabric.Canvas('c');
+
     // var red = new fabric.Rect({
-    //   top: 100, left: 0, width: 80, height: 50, fill: 'red' });
-    //   var red = new fabric.Circle({ top: 140, left: 230, radius: 75, fill: 'green' });
-    // this.canvas.add(red);
 
+    //   top: 100, left: 0, width: 80, height: 50, fill: 'red'
 
-    this.addCenter();
-  //  this.addClient();
-
-
-  }
-
-  addClient() {
-
-    var green =new fabric.Circle({ radius: 40, fill: '#f55', top: 125, left: 125, scaleY: 0.5, flipY: true });
-    var group = new fabric.Group([green], {
-      left: 100,
-      top: 50,
-      angle: -10
-    });
-
-    this.canvas.add(group);
-
-  }
-
-  addCenter() {
-    var circle = new fabric.Circle({
-      radius: 100,
-      fill: '#c2c7cf',
-      originX: 'center',
-      originY: 'center'
-    });
-
-    var circle1 = new fabric.Circle({
-      radius: 95,
-      fill: '#ffffff',
-      originX: 'center',
-      originY: 'center'
-    });
-
-    var text = new fabric.Text('5', {
-      fontSize: 30,
-      originX: 'center',
-      originY: 'center'
-    });
-    var client1 =new fabric.Circle({ radius: 40, fill: '#f55', top: 120, left: 120, });
-
-    var client2 =new fabric.Circle({ radius: 40, fill: '#f55', top: 0, left: 0, });
-
-    var group = new fabric.Group([circle, circle1, text,client1, client2], {
-      left: 150,
-      top: 100,
-      angle: -10
-    });
-
-    this.canvas.add(group);
-
-  }
-  int(): void {
-    this.canvas = new fabric.Canvas('c');
-
-    // let rect = new fabric.Rect({
-    //   width: 50,
-    //   height: 50,
-    //   left: 100,
-    //   top: 100,
-    //   stroke: '#aaf',
-    //   strokeWidth: 5,
-    //   fill: '#faa',
-    //   selectable: false
     // });
-    // this.canvas.add(rect);
+
+    // var blue = new fabric.Rect({
+
+    //   top: 0, left: 100, width: 50, height: 70, fill: 'blue'
+
+    // });
+
+    // var green = new fabric.Rect({
+
+    //   top: 100, left: 100, width: 60, height: 60, fill: 'green'
+
+    // });
+
+    fabric.Object.prototype.transparentCorners = false;
+
+  //  this.canvas.add(red, blue, green);
+
+
+
+// lam mo`
+
+  this.canvas.on({
+
+    'object:moving': function(e) {
+
+      console.log('object:moving :', e);
+
+      e.target.opacity = 0.5;
+
+    },
+
+    'object:modified': function(e) {
+
+      console.log('object:modified :', e);
+
+    },
+
+    'mouse:up': function(e) {
+
+      console.log('mouse:up :', e);
+
+    },
+
+    'mouse:down': function(e) {
+
+      console.log('mouse:down :', e);
+
+    },
+
+    'mouse:move': function(e) {
+
+      console.log('mouse:move :', e);
+
+    },
+
+    'mouse:dblclick': function(e) {
+
+      console.log('mouse:dblclick :', e);
+
+    } 
+
+  });
+
+
+
+
+
+
+
+    this.addShape();
+
+  }
+
+
+
+
+
+
+
+  addShape() {
+
+    var circle =   new fabric.Circle({
+
+      // left: 80,
+
+      // top: 50,
+
+      strokeWidth: 5,
+
+      radius: 50,
+
+      fill: '#fff',
+
+      stroke: '#666',
+
+      originX: 'center',
+
+      originY: 'center'
+
+    });
+
+
+
+    var text = new fabric.Text('9', {
+
+      fontSize: 30,
+
+      originX: 'center',
+
+      originY: 'center'
+
+    }); 
+
+    let listItem = [ circle, text,
+
+    
+
+      ];
+
+    var items = 6;
+
+    for(var i = 0; i < items; i++) {
+
+        var x0 = -25, y0 =-25, r = 100;
+
+        var x = x0 + r * Math.cos(2 * Math.PI * i / items);
+
+        var y = y0 + r * Math.sin(2 * Math.PI * i / items);    
+
+        var child1 =   new fabric.Circle({
+
+          left: x,
+
+          top: y,
+
+          strokeWidth: 5,
+
+          radius: 25,
+
+          fill: '#fff',
+
+          stroke: '#666'
+
+        });
+
+        let ix = i+1+'';
+
+
+
+        var text1 = new fabric.Text(ix, {
+
+          fontSize: 30,
+
+          left: x+15,
+
+          top: y+15
+
+        }); 
+
+
+
+        listItem.push(child1);
+
+        listItem.push(text1);
+
+    }
+
+   
+
+    var group = new fabric.Group(listItem, {
+
+      left: 150,
+
+      top: 100,
+
+      angle: -10
+
+    });
+
+
+
+
+
+    this.canvas.add(group);
+
+  }
+
+
+
+  save() {
+
+    let data =  JSON.stringify(this.canvas);
+
+    console.log('json data : ', data);
+
+    localStorage.setItem('data',data);
+
+  }
+
+  load () {
+
+    let data = JSON.parse(localStorage.getItem('data'));
+
+    this.canvas.loadFromJSON(data);
+
   }
 
 }
+
